1a667fe34f2de917aa5a7a79b3a0d824a16d3f4a
manish7479dlp <manish7479dlp@gmail.com>
2023-12-22 16:42:57 +0530

    gfg: Recursively remove all adjacent duplicates string question sol

--- Day2.java
+++ Day2.java
@@ -1,18 +1,34 @@
-// Minimize the Heights I
+//Recursively remove all adjacent duplicates
+class Day2{
+    String rremove(String s) {
+        if(s.isEmpty()) return s;
+        StringBuilder ans = new StringBuilder();
+        helper(ans, s);
+        return ans.toString();
+    }
+    
+      void helper(StringBuilder sb, String s){
+        boolean [] adjacentFlag = new boolean[s.length()];
+        //tag adjacent as true;
+        for(int i = 1; i<s.length(); i++){
+            if(s.charAt(i -1) == s.charAt(i)){
+                adjacentFlag[i] = adjacentFlag[i - 1] =true;
+            }
+        }
+      
+        StringBuilder rem = new StringBuilder();
+        //find remaing elements not tagged as true
+        for(int i =0 ;i< adjacentFlag.length; i++){
+            if(!adjacentFlag[i]) rem.append(s.charAt(i));
+        }
+      
+        //0 Adjacent element(s) exist, return!
+        if(rem.length() == s.length()){
+            sb.append(s);
+            return;
+        }
+        helper(sb, rem.toString());
+    }
 
-class Day2 {
-    int getMinDiff(int[] arr, int n, int k) {
-        // code here
-     if(n==1) return 0;
-     Arrays.sort(arr);
-     int diff=arr[n-1]-arr[0];
-     int max;
-     int min;
-     
-     for(int i=1;i<n;i++){
-         max=Math.max(arr[i-1]+k,arr[n-1]-k);
-         min=Math.min(arr[0]+k,arr[i]-k);
-         diff=Math.min(diff,max-min);
-     }
-     return diff;
-    }}
+
+}
