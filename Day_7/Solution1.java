ef7199feb9030255c39eee86a954be23b840bc97
manish7479dlp <manish7479dlp@gmail.com>
2023-12-27 13:09:39 +0530

    gfg: Maximize sum(arr[i]*i) of an Array solution

---
+++ Day_7/Solution1.java
@@ -0,0 +1,22 @@
+//Maximize sum(arr[i]*i) of an Array
+
+class Solution{
+    
+    int Maximize(int arr[], int n)
+    {
+        // Complete the function
+        Arrays.sort(arr);
+
+        long ans=0;
+
+        for(long i=0;i<n;i++){
+
+            ans+=(arr[(int)i]*i);
+
+            ans=ans%1000000007;
+
+        }
+
+        return (int)ans%1000000007;
+    }   
+}
